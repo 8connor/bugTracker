122540e1a6e6ba30af27495d81f0bac3ae2b576e
The-Napster <connorh16@gmail.com>
2021-02-14 18:49:03 -0500

    Have the id of the the specific ticket coming into the images collection now.

--- backEnd/Routes/Tickets.js
+++ backEnd/Routes/Tickets.js
@@ -6,81 +6,78 @@ const db = require("../Models/index");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'images');
+        cb(null, "public/images");
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
         cb(null, true);
     } else {
         cb(null, false);
     }
-}
+};
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 // ========= Get routes ============
 Tickets.get("/tickets", (req, res) => {
-    db.Tickets
-        .find({})
+    db.Tickets.find({})
         .lean()
-        .then(response => res.json(response))
-        .catch(err => console.log(err))
+        .then((response) => res.json(response))
+        .catch((err) => console.log(err));
 });
 
 // ========= Post routes ============
 
 Tickets.post("/makeTicket", (req, res) => {
-    console.log("hit")
     const { project, description, severity } = req.body;
 
-    db.Images.create({project: project}).then(data => console.log(data)).catch(err => console.log(err))
+    db.Images.create({ project: project })
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
 
-    db.Tickets
-        .create({
-            project: project,
-            description: description,
-            severity: severity
-        })
-        .then(response => res.json(response))
-        .catch(err => console.log(err))
+    db.Tickets.create({
+        project: project,
+        description: description,
+        severity: severity,
+    })
+        .then((response) => res.json(response))
+        .catch((err) => console.log(err));
 });
 
 //Upload route
-Tickets.post('/upload', upload.single('image'), (req, res, next) => {
+Tickets.post("/upload", upload.single("image"), (req, res, next) => {
     console.log("reaching the route");
-    console.log(req.body.projName);
-    console.log(req.file.filename);
+    // console.log(req.body.projName);
+    console.log(req.body.ticketId);
+    // console.log(req.file.filename);
 
     db.Projects.findOne({ name: req.body.projName })
         .lean()
-        .then(obj => {
+        .then((obj) => {
             if (obj) {
                 db.Images.updateOne(
                     { project: req.body.projName },
                     {
                         $push: {
                             images: {
-                                location: `images/${req.file.filename}`
-                            }
-                        }
+                                location: `public/images/${req.file.filename}`,
+                            },
+                        },
                     }
                 )
-                    .then(data => console.log(data))
-                    .catch(err => console.log(err));
+                    .then((data) => console.log(data))
+                    .catch((err) => console.log(err));
+            } else {
+                return;
             }
-            else {
-                return
-            }
-        }).catch(err => console.log(err))
+        })
+        .catch((err) => console.log(err));
     // Creating a new collection to handle images and their locations.
-
-
 });
 
-
-module.exports = Tickets
+module.exports = Tickets;
