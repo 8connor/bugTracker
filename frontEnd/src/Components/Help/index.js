6b7594d61d6ddc3cd2571677540708996cd37366
8connor <connorh16@gmail.com>
2021-02-16 14:05:59 -0500

    Working on the help section.

--- frontEnd/src/Components/Help/index.js
+++ frontEnd/src/Components/Help/index.js
@@ -3,17 +3,30 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import "./index.css";
 
-
-function Help(){
-    return(
+function Help() {
+    return (
         <Container>
             <Row className="justify-content-center">
-                <Col>
-                    <p>
-                        For assistance please contact me via github, linkedIn, or email
-                    </p>
-
+                <Col
+                    xs={12}
+                    sm={12}
+                    md={12}
+                    lg={12}
+                >
+                    <Card
+                        className="shadow"
+                        style={{height: "50vh", marginTop:"50px", marginBottom: "50vh"}}
+                    >
+                        <Card.Body
+                            className="text-center mt-5 aboutCard"
+                        >
+                            <p>
+                                For assistance please contact me via github, linkedIn, or email.
+                            </p>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
         </Container>
