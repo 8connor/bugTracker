6ce49932e0c64b4d7898d8f014a2546ddb64a227
8connor <connorh16@gmail.com>
2021-02-17 10:59:33 -0500

    Getting ready for heroku deployment.

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const { Projects, Users, Tickets } = require("./Routes/index.js");
 
-mongoose.connect(
-    `mongodb://localhost:27017/bugTracker`,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("Successfully connected to Database");
-    }
-);
+mongoose.connect(`mongodb+srv://herokuUser:${process.env.herokuPass}@cluster0.beusi.mongodb.net/`, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Successfully connected to Database");
+});
 
 app.use(express.json());
 app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 app.use("/api", Projects);
 app.use("/api", Users);
